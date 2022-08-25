21ba1b2c62a2af1d3bafa16bfdce0ffba3f6de52
coding-kill <342957950@qq.com>
2022-08-25 16:05:17 +0800

    增加查询指定路径重复文件的方法

--- code-admin/src/main/java/com/code/util/FileUtils.java
+++ code-admin/src/main/java/com/code/util/FileUtils.java
@@ -28,49 +28,70 @@ import java.util.Set;
 public class FileUtils {
 
 //  查询给定路径内的重复文件
-    public static void main(String[] args) {
-        Set<String> md5Set = new HashSet<>();
-        Map<String,Object> md5Map = new HashMap<>();
-        String path = "E:\\";
-        File file = new File(path);
-        if(file.isDirectory()){
-            getDirectoryChildMd5(path, md5Set,md5Map);
-        }else {
-            try {
-                String md5 = DigestUtils.md5Hex(new FileInputStream(file.getAbsolutePath()));
-                if(md5Set.contains(md5)){
-                    System.out.println("当前文件重复，文件名--》"+file.getAbsolutePath()+";已有文件路径--》"+md5Map.get(md5));
-                }else {
-                    md5Set.add(md5);
-                    md5Map.put(md5,file.getAbsolutePath());
-                }
-            }catch (Exception e){
-                e.printStackTrace();
+public static void main(String[] args) {
+    Set<String> md5Set = new HashSet<>();
+    Map<String,Object> md5Map = new HashMap<>();
+    String path = "E:\\";
+    File file = new File(path);
+    if(file.isDirectory()){
+        getDirectoryChildMd5(path, md5Set,md5Map);
+    }else {
+        try {
+            if(file.length()==0){
+                return;
             }
+            String md5 = DigestUtils.md5Hex(new FileInputStream(file.getAbsolutePath()));
+//				采用MD5加文件大小
+            md5 = md5 + file.length();
+            if(md5Set.contains(md5)){
+                System.out.println("当前文件重复，文件名--》"+file.getAbsolutePath()+";已有文件路径--》"+md5Map.get(md5));
+                FileUtil.writeFile("当前文件重复，文件名--》"+file.getAbsolutePath()+";已有文件路径--》"+md5Map.get(md5)+ System.getProperty("line.separator"),
+                        "E:\\","chongfu.txt","UTF-8");
+            }else {
+                md5Set.add(md5);
+                md5Map.put(md5,file.getAbsolutePath());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
+}
     public static void getDirectoryChildMd5(String path,Set<String> md5Set,Map<String,Object> md5Map){
         File file = new File(path);
         File[] files = file.listFiles();
-        for (File getFile : files) {
-            String currentPath = getFile.toPath().toString();
-            currentPath = currentPath.replaceAll("\\\\", "/");
-            if(getFile.isDirectory()){
-                getDirectoryChildMd5(currentPath, md5Set,md5Map);
-            }else {
-                try {
-                    String md5 = DigestUtils.md5Hex(new FileInputStream(getFile.getAbsolutePath()));
-                    if(md5Set.contains(md5)){
-                        System.out.println("当前文件重复，文件名--》"+getFile.getAbsolutePath()+";已有文件路径--》"+md5Map.get(md5));
-                    }else {
-                        md5Set.add(md5);
-                        md5Map.put(md5,getFile.getAbsolutePath());
+        try{
+            for (File getFile : files) {
+                String currentPath = getFile.toPath().toString();
+                currentPath = currentPath.replaceAll("\\\\", "/");
+                if(getFile.isDirectory()){
+                    getDirectoryChildMd5(currentPath, md5Set,md5Map);
+                }else {
+                    try {
+                        if(file.length()==0){
+                            continue;
+                        }
+                        String md5 = DigestUtils.md5Hex(new FileInputStream(getFile.getAbsolutePath()));
+                        md5 = md5 + file.length();
+                        if(md5Set.contains(md5)){
+                            System.out.println("当前文件重复，文件名--》"+getFile.getAbsolutePath()+";已有文件路径--》"+md5Map.get(md5));
+                            FileUtil.writeFile("当前文件重复，文件名--》"+getFile.getAbsolutePath()+";已有文件路径--》"+md5Map.get(md5)+ System.getProperty("line.separator"),
+                                    "E:\\","chongfu.txt","UTF-8");
+                        }else {
+                            md5Set.add(md5);
+                            md5Map.put(md5,getFile.getAbsolutePath());
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
                     }
-                }catch (Exception e){
-                    e.printStackTrace();
                 }
             }
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("错误路径="+path);
+            FileUtil.writeFile("错误路径="+path+ System.getProperty("line.separator"),
+                    "E:\\","error.txt","UTF-8");
         }
+
     }
 
 }
